3a98cdf2e44bcf7fa8ea0661c3c7586af54105aa
galales <25105748+galales@users.noreply.github.com>
2022-04-20 12:44:36 +0200

    pin-1382: Different audience for client assertion

--- kubernetes/configs/dev/authorization_server.sh
+++ kubernetes/configs/dev/authorization_server.sh
@@ -7,3 +7,5 @@ INTERNAL_JWT_ISSUER="dev.interop.pagopa.it"
 INTERNAL_JWT_SUBJECT="dev.interop-m2m"
 INTERNAL_JWT_AUDIENCE="dev.interop.pagopa.it/m2m"
 INTERNAL_JWT_DURATION_SECONDS=3600
+
+CLIENT_ASSERTION_JWT_AUDIENCE="dev.interop.pagopa.it/client-assertion"
--- kubernetes/configs/test/authorization_server.sh
+++ kubernetes/configs/test/authorization_server.sh
@@ -7,3 +7,5 @@ INTERNAL_JWT_ISSUER="test.interop.pagopa.it"
 INTERNAL_JWT_SUBJECT="test.interop-m2m"
 INTERNAL_JWT_AUDIENCE="test.interop.pagopa.it/m2m"
 INTERNAL_JWT_DURATION_SECONDS=3600
+
+CLIENT_ASSERTION_JWT_AUDIENCE="test.interop.pagopa.it/client-assertion"
