8063787f9b1cf9d7b4140b85fe9565450ccae241
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-04-22 18:03:19 +0200

    add BFF deploy

---
+++ kubernetes/configs/dev/backend_for_frontend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+INTEROP_JWT_DURATION=99999999
+INTEROP_JWT_ISSUER="dev.interop.pagopa.it"
---
+++ kubernetes/configs/test/backend_for_frontend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+INTEROP_JWT_DURATION=600
+INTEROP_JWT_ISSUER="test.interop.pagopa.it"
