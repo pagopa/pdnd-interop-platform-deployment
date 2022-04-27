ec51f65907b31c5c15c597fddb0a71a8c55dc1f2
Antonio Gelameris <toniogela89@gmail.com>
2022-04-27 17:03:56 +0200

    Added SQS configurations to purpose and agreement management

--- kubernetes/configs/test/commons.sh
+++ kubernetes/configs/test/commons.sh
@@ -12,7 +12,9 @@ REPOSITORY="505630707203.dkr.ecr.eu-central-1.amazonaws.com"
 POSTGRES_HOST="pdnd-interop-test-rds.cfx5ud7lsyvt.eu-central-1.rds.amazonaws.com"
 POSTGRES_PORT="5432"
 REPLICAS=1
+PERSISTENCE_EVENTS_QUEUE_URL="https://sqs.us-east-1.amazonaws.com/895646477129/persistence-events.fifo"
+AWS_REGION="eu-central-1"
 
 MAIN_AUDIENCE="test.interop.pagopa.it"
-ENABLED_PROJECTIONS="false"
+ENABLED_PROJECTIONS="true"
 WELL_KNOWN_URL="https://pdnd-interop-test-public.s3.eu-central-1.amazonaws.com/.well-known/jwks.json"
