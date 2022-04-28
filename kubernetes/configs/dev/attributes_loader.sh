276ab0e356f7ee915f65bbd9d429c0b6f73938bf
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 10:22:39 +0200

    pin-1382: Different audiences for client assertion, m2m, ui and internal (#80)

--- kubernetes/configs/dev/attributes_loader.sh
+++ kubernetes/configs/dev/attributes_loader.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ATTRIBUTES_LOADER_TOKEN_JWT_ISSUER="interop.pagopa.it"
-ATTRIBUTES_LOADER_TOKEN_JWT_SUBJECT="interop-attributes-loader"
-ATTRIBUTES_LOADER_TOKEN_JWT_AUDIENCE="interop.pagopa.it/attributes-loader"
+ATTRIBUTES_LOADER_TOKEN_JWT_ISSUER="dev.interop.pagopa.it"
+ATTRIBUTES_LOADER_TOKEN_JWT_SUBJECT="dev.interop-attributes-loader"
 ATTRIBUTES_LOADER_TOKEN_JWT_DURATION_SECONDS=3600
--- kubernetes/configs/test/commons.sh
+++ kubernetes/configs/test/commons.sh
@@ -13,6 +13,9 @@ POSTGRES_HOST="pdnd-interop-test-rds.cfx5ud7lsyvt.eu-central-1.rds.amazonaws.com
 POSTGRES_PORT="5432"
 REPLICAS=1
 
-MAIN_AUDIENCE="test.interop.pagopa.it"
 ENABLED_PROJECTIONS="false"
 WELL_KNOWN_URL="https://pdnd-interop-test-public.s3.eu-central-1.amazonaws.com/.well-known/jwks.json"
+
+UI_JWT_AUDIENCE="test.interop.pagopa.it/ui"
+M2M_JWT_AUDIENCE="test.interop.pagopa.it/m2m"
+INTERNAL_JWT_AUDIENCE="test.interop.pagopa.it/internal"
