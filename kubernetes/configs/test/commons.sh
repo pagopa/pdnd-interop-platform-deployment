046606586e24a9ea64ab65a76b5c81c7bb3650ca
Antonio Gelameris <toniogela89@gmail.com>
2022-04-28 08:59:52 +0200

    Fixed queue url

--- kubernetes/configs/test/commons.sh
+++ kubernetes/configs/test/commons.sh
@@ -12,7 +12,7 @@ REPOSITORY="505630707203.dkr.ecr.eu-central-1.amazonaws.com"
 POSTGRES_HOST="pdnd-interop-test-rds.cfx5ud7lsyvt.eu-central-1.rds.amazonaws.com"
 POSTGRES_PORT="5432"
 REPLICAS=1
-PERSISTENCE_EVENTS_QUEUE_URL="https://sqs.us-east-1.amazonaws.com/895646477129/persistence-events.fifo"
+PERSISTENCE_EVENTS_QUEUE_URL="https://sqs.eu-central-1.amazonaws.com/895646477129/persistence-events.fifo"
 AWS_REGION="eu-central-1"
 
 MAIN_AUDIENCE="test.interop.pagopa.it"
