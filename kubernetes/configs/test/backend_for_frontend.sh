addd27b333c462e0e56fd22a9f6d5de255b0db47
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-05-04 15:59:24 +0200

    missing commit

--- kubernetes/configs/test/backend_for_frontend.sh
+++ kubernetes/configs/test/backend_for_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INTEROP_SESSION_TOKEN_DURATION_SECONDS=600
+INTEROP_SESSION_TOKEN_DURATION_SECONDS=86400
 INTEROP_SESSION_TOKEN_ISSUER="test.interop.pagopa.it"
 SELFCARE_AUDIENCE="fe-test.gateway-private.test.pdnd-interop.pagopa.it"
 SELFCARE_WELL_KNOWN_URL="https://uat.selfcare.pagopa.it/.well-known/jwks.json"
