0a9c6bfea4f62e32f3ee8810ea67e07b860481bc
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-05-27 11:57:39 +0200

    selfcare Integration (#96)
    * update backend-for-frontend with selfcare deps
    
    * selfcare integration completed
    
    * clean README
    
    * fix bff configmap
    
    * apply some fixes
    
    * fix Jenkinsfile
    
    * apply PR comments

--- kubernetes/configs/dev/commons.sh
+++ kubernetes/configs/dev/commons.sh
@@ -15,7 +15,7 @@ PERSISTENCE_QUEUE_NAME="${NAMESPACE}-persistence-events.fifo"
 AWS_SQS_DOMAIN="https://sqs.${AWS_REGION}.amazonaws.com"
 
 ENABLED_PROJECTIONS="true"
-WELL_KNOWN_URL="https://pdnd-interop-dev-public.s3.eu-central-1.amazonaws.com/.well-known/jwks.json"
+WELL_KNOWN_URLS="https://pdnd-interop-dev-public.s3.eu-central-1.amazonaws.com/.well-known/jwks.json"
 
 UI_JWT_AUDIENCE="dev.interop.pagopa.it/ui"
 M2M_JWT_AUDIENCE="dev.interop.pagopa.it/m2m"
@@ -23,3 +23,11 @@ INTERNAL_JWT_AUDIENCE="dev.interop.pagopa.it/internal"
 EC_KEYS_IDENTIFIERS="interop-ecdsa-p256-01"
 RSA_KEYS_IDENTIFIERS="interop-rsa4096-01"
 VAULT_SIGNATURE_ROUTE="/v1/transit/sign/"
+
+PARTY_PROCESS_URL="https://api.dev.selfcare.pagopa.it/external/party-process/v1"
+PARTY_MANAGEMENT_URL="https://api.dev.selfcare.pagopa.it/external/party-management/v1"
+USER_REGISTRY_URL="https://api.uat.pdv.pagopa.it/user-registry/v1"
+
+INTERNAL_JWT_ISSUER="dev.interop.pagopa.it"
+INTERNAL_JWT_SUBJECT="dev.interop-m2m"
+INTERNAL_JWT_DURATION_SECONDS=3600
