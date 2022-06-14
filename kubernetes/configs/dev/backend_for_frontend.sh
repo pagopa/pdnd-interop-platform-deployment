1fe55342192b507cca2caa60b8bdc4e52787c2e3
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-06-14 15:13:56 +0200

    fix selfcare audience

--- kubernetes/configs/dev/backend_for_frontend.sh
+++ kubernetes/configs/dev/backend_for_frontend.sh
@@ -3,5 +3,5 @@
 INTEROP_SESSION_TOKEN_DURATION_SECONDS=99999999
 INTEROP_SESSION_TOKEN_ISSUER="dev.interop.pagopa.it"
 
-SELFCARE_AUDIENCE="fe-test.gateway-private.test.pdnd-interop.pagopa.it"
+SELFCARE_AUDIENCE="api.interop.selfcare.pagopa.it"
 SELFCARE_WELL_KNOWN_URL="https://uat.selfcare.pagopa.it/.well-known/jwks.json"
