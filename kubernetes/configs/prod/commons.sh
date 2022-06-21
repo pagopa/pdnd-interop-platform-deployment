c061a27ab75d31fc69d7fa8cab4b153097d55336
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-06-21 14:59:58 +0200

    fix prod WELL_KNOWN_URLS

--- kubernetes/configs/prod/commons.sh
+++ kubernetes/configs/prod/commons.sh
@@ -13,7 +13,7 @@ PERSISTENCE_QUEUE_NAME="persistence-events.fifo"
 AWS_SQS_DOMAIN="https://sqs.${AWS_REGION}.amazonaws.com"
 
 ENABLED_PROJECTIONS="true"
-WELL_KNOWN_URLS="https://interop.uat.selfcare.pagopa.it/.well-known/jwks.json"
+WELL_KNOWN_URLS="https://interop.selfcare.pagopa.it/.well-known/jwks.json"
 
 INTERNAL_JWT_ISSUER="prod.interop.pagopa.it"
 INTERNAL_JWT_SUBJECT="prod.interop-m2m"
