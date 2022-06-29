4a19188b245c448e8b08923bfa499b6cad463b7f
galales <25105748+galales@users.noreply.github.com>
2022-06-29 14:49:23 +0200

    pin-1578: New dev env configs

--- kubernetes/configs/dev/commons.sh
+++ kubernetes/configs/dev/commons.sh
@@ -3,7 +3,7 @@
 DOMAIN_NAME="interop.dev.selfcare.pagopa.it"
 
 REPOSITORY="505630707203.dkr.ecr.eu-central-1.amazonaws.com"
-POSTGRES_HOST="pdnd-interop-dev-rds.c9zr6t2swdpb.eu-central-1.rds.amazonaws.com"
+POSTGRES_HOST="interop-rds-dev-auroradbcluster-1ws49gkll6459.cluster-c9zr6t2swdpb.eu-central-1.rds.amazonaws.com"
 POSTGRES_PORT="5432"
 REPLICAS=1
 BACKEND_SERVICE_PORT="8088"
@@ -15,17 +15,16 @@ AWS_SQS_DOMAIN="https://sqs.${AWS_REGION}.amazonaws.com"
 ENABLED_PROJECTIONS="true"
 WELL_KNOWN_URLS="https://interop.dev.selfcare.pagopa.it/.well-known/jwks.json"
 
+INTERNAL_JWT_ISSUER="dev.interop.pagopa.it"
+INTERNAL_JWT_SUBJECT="dev.interop-m2m"
+INTERNAL_JWT_DURATION_SECONDS=3600
+
 UI_JWT_AUDIENCE="dev.interop.pagopa.it/ui"
 M2M_JWT_AUDIENCE="dev.interop.pagopa.it/m2m"
 INTERNAL_JWT_AUDIENCE="dev.interop.pagopa.it/internal"
-EC_KEYS_IDENTIFIERS="interop-ecdsa-p256-01"
-RSA_KEYS_IDENTIFIERS="interop-rsa4096-01"
+RSA_KEYS_IDENTIFIERS="interop-rsa2048-dev"
 VAULT_SIGNATURE_ROUTE="/v1/transit/sign/"
 
 PARTY_PROCESS_URL="https://api.dev.selfcare.pagopa.it/external/party-process/v1"
 PARTY_MANAGEMENT_URL="https://api.dev.selfcare.pagopa.it/external/party-management/v1"
 USER_REGISTRY_URL="https://api.uat.pdv.pagopa.it/user-registry/v1"
-
-INTERNAL_JWT_ISSUER="dev.interop.pagopa.it"
-INTERNAL_JWT_SUBJECT="dev.interop-m2m"
-INTERNAL_JWT_DURATION_SECONDS=3600
--- kubernetes/configs/dev/frontend.sh
+++ kubernetes/configs/dev/frontend.sh
@@ -2,5 +2,6 @@
 
 SELFCARE_LOGIN_URL="https://dev.selfcare.pagopa.it/auth/login"
 FRONTEND_SERVICE_PORT="80"
-PUBLIC_BUCKET_URL="https://pdnd-interop-dev-public.s3.eu-central-1.amazonaws.com"
-MIXPANEL_PROJECT_ID=""
+PUBLIC_BUCKET_URL="https://interop-dev-public.s3.eu-central-1.amazonaws.com"
+MIXPANEL_PROJECT_ID="fill me"
+ONETRUST_DOMAIN_SCRIPT_ID="fill me"
