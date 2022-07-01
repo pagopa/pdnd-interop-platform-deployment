3a8164de5c917c04bb9f20c626bea7e9aefc06c6
galales <25105748+galales@users.noreply.github.com>
2022-07-01 11:56:37 +0200

    configs-tuning: Use one version file for each env

--- kubernetes/configs/prod/main.sh
+++ kubernetes/configs/prod/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 . $(pwd)/kubernetes/configs/shared.sh
-. $(pwd)/kubernetes/configs/versions.sh
 
+. $(pwd)/kubernetes/configs/prod/versions.sh
 . $(pwd)/kubernetes/configs/prod/commons.sh
 
 
