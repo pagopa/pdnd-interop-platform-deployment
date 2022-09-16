dedf02adc82ceb42b3938b751eed7477f7355277
galales <25105748+galales@users.noreply.github.com>
2022-09-16 17:45:33 +0200

    test-release-20220916: Test configs for release 20220916

--- kubernetes/configs/test/main.sh
+++ kubernetes/configs/test/main.sh
@@ -5,7 +5,6 @@
 . $(pwd)/kubernetes/configs/test/versions.sh
 . $(pwd)/kubernetes/configs/test/commons.sh
 
-
 # Calculated
 NAMESPACE=$NAMESPACE
 EXTERNAL_APPLICATION_HOST=$DOMAIN_NAME
@@ -17,14 +16,17 @@ CATALOG_MANAGEMENT_INTERFACE_VERSION=$(shortVersion $CATALOG_MANAGEMENT_IMAGE_VE
 PARTY_REGISTRY_PROXY_INTERFACE_VERSION=$(shortVersion $PARTY_REGISTRY_PROXY_IMAGE_VERSION)
 PARTY_MOCK_REGISTRY_INTERFACE_VERSION=$(shortVersion $PARTY_MOCK_REGISTRY_IMAGE_VERSION)
 PURPOSE_MANAGEMENT_INTERFACE_VERSION=$(shortVersion $PURPOSE_MANAGEMENT_IMAGE_VERSION)
+TENANT_MANAGEMENT_INTERFACE_VERSION=$(shortVersion $TENANT_MANAGEMENT_IMAGE_VERSION)
 
-API_GATEWAY_INTERFACE_VERSION=$(shortVersion $API_GATEWAY_IMAGE_VERSION)
 AGREEMENT_PROCESS_INTERFACE_VERSION=$(shortVersion $AGREEMENT_PROCESS_IMAGE_VERSION)
 AUTHORIZATION_PROCESS_INTERFACE_VERSION=$(shortVersion $AUTHORIZATION_PROCESS_IMAGE_VERSION)
 CATALOG_PROCESS_INTERFACE_VERSION=$(shortVersion $CATALOG_PROCESS_IMAGE_VERSION)
 PURPOSE_PROCESS_INTERFACE_VERSION=$(shortVersion $PURPOSE_PROCESS_IMAGE_VERSION)
-NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
+TENANT_PROCESS_INTERFACE_VERSION=$(shortVersion $TENANT_PROCESS_IMAGE_VERSION)
+
+API_GATEWAY_INTERFACE_VERSION=$(shortVersion $API_GATEWAY_IMAGE_VERSION)
 BACKEND_FOR_FRONTEND_INTERFACE_VERSION=$(shortVersion $BACKEND_FOR_FRONTEND_IMAGE_VERSION)
+NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 
 . $(pwd)/kubernetes/configs/test/agreement_management.sh
 . $(pwd)/kubernetes/configs/test/agreement_process.sh
@@ -42,5 +44,8 @@ BACKEND_FOR_FRONTEND_INTERFACE_VERSION=$(shortVersion $BACKEND_FOR_FRONTEND_IMAG
 . $(pwd)/kubernetes/configs/test/party_mock_registry.sh
 . $(pwd)/kubernetes/configs/test/purpose_management.sh
 . $(pwd)/kubernetes/configs/test/purpose_process.sh
+. $(pwd)/kubernetes/configs/test/tenant_management.sh
+. $(pwd)/kubernetes/configs/test/tenant_process.sh
+. $(pwd)/kubernetes/configs/test/tenants-certified-attributes-updater.sh
 . $(pwd)/kubernetes/configs/test/notifier.sh
 . $(pwd)/kubernetes/configs/test/token_details_persister.sh
