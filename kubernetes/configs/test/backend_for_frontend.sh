2b8a434298260dbaaa0ea1c51f2d71d5bcdbefe6
galales <25105748+galales@users.noreply.github.com>
2022-10-14 14:44:24 +0200

    pin-2018: Update selfcare urls and audiences

--- kubernetes/configs/test/backend_for_frontend.sh
+++ kubernetes/configs/test/backend_for_frontend.sh
@@ -3,8 +3,8 @@
 INTEROP_SESSION_TOKEN_DURATION_SECONDS=3600
 INTEROP_SESSION_TOKEN_ISSUER="uat.interop.pagopa.it"
 
-SELFCARE_AUDIENCE="api.interop.selfcare.pagopa.it"
-SELFCARE_WELL_KNOWN_URL="https://uat.selfcare.pagopa.it/.well-known/jwks.json"
+SELFCARE_AUDIENCE="selfcare.uat.interop.pagopa.it"
+SELFCARE_WELL_KNOWN_URL="https://selfcare.pagopa.it/.well-known/jwks.json"
 
 BACKEND_FOR_FRONTEND_RESOURCE_CPU="500m"
 BACKEND_FOR_FRONTEND_RESOURCE_MEM="2Gi"
