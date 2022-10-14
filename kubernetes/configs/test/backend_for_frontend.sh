251cb49688dd93d4d0e27bed9703ec4ce81bad46
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-10-14 15:00:36 +0200

    pin-2017: Update domains (#155)

--- kubernetes/configs/test/backend_for_frontend.sh
+++ kubernetes/configs/test/backend_for_frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INTEROP_SESSION_TOKEN_DURATION_SECONDS=3600
-INTEROP_SESSION_TOKEN_ISSUER="test.interop.pagopa.it"
+INTEROP_SESSION_TOKEN_ISSUER="uat.interop.pagopa.it"
 
 SELFCARE_AUDIENCE="api.interop.selfcare.pagopa.it"
 SELFCARE_WELL_KNOWN_URL="https://uat.selfcare.pagopa.it/.well-known/jwks.json"
