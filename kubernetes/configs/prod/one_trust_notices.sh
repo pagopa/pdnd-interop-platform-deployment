3c158119c8c3a79d36856390b5ff6c1d2460efee
Antonio Gelameris <toniogela89@gmail.com>
2023-07-05 16:26:24 +0200

    Adding one-trust-job

---
+++ kubernetes/configs/prod/one_trust_notices.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LANGS=it
+PUBLIC_STORAGE_PATH=one-trust-notices
+CONTENT_STORAGE_BUCKET=interop-privacy-notices-content-prod
+HISTORY_STORAGE_BUCKET=interop-privacy-notices-history-prod
+
+JOB_ONE_TRUST_NOTICES_RESOURCE_CPU="500m"
+JOB_ONE_TRUST_NOTICES_RESOURCE_MEM="1Gi"
