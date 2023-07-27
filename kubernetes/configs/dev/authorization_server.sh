acf4e4ed88080a4329981594d8380f5a03ff2070
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-07-27 11:31:40 +0200

    - restore auth-server INTEROP_JWT_DURATION_SECONDS in dev  - reduce bff INTEROP_SESSION_TOKEN_DURATION_SECONDS in dev

--- kubernetes/configs/dev/authorization_server.sh
+++ kubernetes/configs/dev/authorization_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INTEROP_JWT_DURATION_SECONDS=120
+INTEROP_JWT_DURATION_SECONDS=99999999
 INTEROP_JWT_ISSUER="dev.interop.pagopa.it"
 
 CLIENT_ASSERTION_JWT_AUDIENCE="auth.dev.interop.pagopa.it/client-assertion"
