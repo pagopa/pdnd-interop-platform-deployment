795e2e4c8e6dc3a514fc013407fe66977a62fc95
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-08-29 11:48:03 +0200

    PIN-3819-3: Fix variables names for Selfcare Onboarding Consumer

--- kubernetes/configs/dev/selfcare_onboarding_consumer.sh
+++ kubernetes/configs/dev/selfcare_onboarding_consumer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SELFCARE_BROKER_URLS="selc-u-eventhub-ns.servicebus.windows.net:9093"
-KAFKA_CLIENT_ID="interop-dev"
-KAFKA_GROUP_ID="interop-onboarding-dev"
-TOPIC_NAME="sc-contracts"
+SELFCARE_CONTRACTS_TOPIC_CLIENT_ID="interop-dev"
+SELFCARE_CONTRACTS_TOPIC_GROUP_ID="interop-onboarding-dev"
+SELFCARE_CONTRACTS_TOPIC_NAME="sc-contracts"
 
 SELFCARE_ONBOARDING_CONSUMER_RESOURCE_CPU="250m"
 SELFCARE_ONBOARDING_CONSUMER_RESOURCE_MEM="512Mi"
