0d3ebab76b4ad11e745f7863d3ec765565f9626a
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-08-30 10:42:18 +0200

    PIN-3819-4: Added selfcare broker url for test and prod (#326)

--- kubernetes/configs/test/selfcare_onboarding_consumer.sh
+++ kubernetes/configs/test/selfcare_onboarding_consumer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SELFCARE_BROKER_URLS="???" # TBD
+SELFCARE_BROKER_URLS="selc-p-eventhub-ns.servicebus.windows.net:9093"
 SELFCARE_CONTRACTS_TOPIC_CLIENT_ID="interop-uat"
 SELFCARE_CONTRACTS_TOPIC_GROUP_ID="interop-onboarding-uat"
 SELFCARE_CONTRACTS_TOPIC_NAME="sc-contracts"
