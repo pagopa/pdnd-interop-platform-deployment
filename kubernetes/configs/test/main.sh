83f503f868a8453b83dcd3f33dbaf974d7692ce9
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2023-12-11 14:20:17 +0100

    PIN-4249: Add ivass-certified-attributes-importer job (#376)

--- kubernetes/configs/test/main.sh
+++ kubernetes/configs/test/main.sh
@@ -58,6 +58,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/test/eservices_monitoring_exporter.sh
 . $(pwd)/kubernetes/configs/test/metrics_report_generator.sh
 . $(pwd)/kubernetes/configs/test/anac_certified_attributes_importer.sh
+. $(pwd)/kubernetes/configs/test/ivass_certified_attributes_importer.sh
 . $(pwd)/kubernetes/configs/test/pn_consumers.sh
 . $(pwd)/kubernetes/configs/test/padigitale_report_generator.sh
 . $(pwd)/kubernetes/configs/test/dashboard_metrics_report_generator.sh
