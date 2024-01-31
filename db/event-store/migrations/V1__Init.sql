a18b17d0548d1ceecf08b31c3a3ba221b0b91735
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2024-01-31 12:38:15 +0100

    IMN-62: Migration for new event store tables

---
+++ db/event-store/migrations/V1__Init.sql
@@ -0,0 +1,14 @@
+CREATE TABLEIF NOT EXISTS "${applicationSchema}".events (
+    sequence_num BIGSERIAL NOT NULL,
+
+    stream_id UUID NOT NULL,
+    version BIGINT NOT NULL,
+
+    type TEXT NOT NULL,
+    data BYTEA NOT NULL,
+
+    log_date TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    PRIMARY KEY (sequence_num),
+    UNIQUE (stream_id, version)
+);
