e3db059224c0d987ec7b2892beee9301499f0934
Michele De Simone <michele.desimone@pagopa.it>
2024-02-09 15:38:43 +0100

    fix string substitution

--- deployment_functions.sh
+++ deployment_functions.sh
@@ -144,11 +144,11 @@ function applyKubeFile() {
   echo "Compiling file $fileName"
 
   SERVICE_NAME="$serviceName" \
-    SERVICE_ECR_NAME="$(echo "$serviceName" | tr '-refactor' '')" \
+    SERVICE_ECR_NAME="$(echo "$serviceName" | sed 's/-refactor//')" \
     IMAGE_DIGEST="$imageDigest" \
     SERVICE_RESOURCE_CPU="$resourceCpu" \
     SERVICE_RESOURCE_MEM="$resourceMem" \
-    LOWERCASE_ENV=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]' | tr '-refactor' '') \
+    LOWERCASE_ENV=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]' | sed 's/-refactor//') \
     ./kubernetes/templater.sh "./kubernetes/$fileName" \
       -s -f "$CONFIG_FILE" \
       > "$compiledFileName"
@@ -174,12 +174,12 @@ function compileDir() {
     if [ ! "$(basename "$f")" = "kustomization.yaml" ]; then
       mkdir -p "${serviceName}/${dirPath}"
       SERVICE_NAME="$serviceName" \
-        SERVICE_ECR_NAME="$(echo "$serviceName" | tr '-refactor' '')" \
+        SERVICE_ECR_NAME="$(echo "$serviceName" | sed 's/-refactor//')" \
         IMAGE_VERSION="$imageVersion" \
         IMAGE_DIGEST="$imageDigest" \
         SERVICE_RESOURCE_CPU="$resourceCpu" \
         SERVICE_RESOURCE_MEM="$resourceMem" \
-        LOWERCASE_ENV=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]' | tr '-refactor' '') \
+        LOWERCASE_ENV=$(echo "$ENVIRONMENT" | tr '[:upper:]' '[:lower:]' | sed 's/-refactor//') \
         ./kubernetes/templater.sh "$f" -s -f "$CONFIG_FILE" \
         > "$serviceName/$f"
     else
