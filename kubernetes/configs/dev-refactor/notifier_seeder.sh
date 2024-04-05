70270096897affcd2c5df92864957d3abc9d2c1a
Michele De Simone <michele.desimone@pagopa.it>
2024-04-05 15:21:26 +0200

    refactor: common topic and queue values

--- kubernetes/configs/dev-refactor/notifier_seeder.sh
+++ kubernetes/configs/dev-refactor/notifier_seeder.sh
@@ -2,4 +2,3 @@
 
 NOTIFIER_SEEDER_RESOURCE_CPU="1000m"
 NOTIFIER_SEEDER_RESOURCE_MEM="2Gi"
-NOTIFICATION_QUEUE_URL="https://sqs.eu-central-1.amazonaws.com/505630707203/persistence-events-refactor.fifo"
