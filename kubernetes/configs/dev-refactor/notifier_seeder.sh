3c030164250df497cd7f9746e8dd5d9209610ef9
Michele De Simone <michele.desimone@pagopa.it>
2024-04-05 14:41:17 +0200

    feat: add notifier-seeder in dev-refactor ENV

---
+++ kubernetes/configs/dev-refactor/notifier_seeder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NOTIFIER_SEEDER_RESOURCE_CPU="1000m"
+NOTIFIER_SEEDER_RESOURCE_MEM="2Gi"
+NOTIFICATION_QUEUE_URL="https://sqs.eu-central-1.amazonaws.com/505630707203/persistence-events-refactor.fifo"
