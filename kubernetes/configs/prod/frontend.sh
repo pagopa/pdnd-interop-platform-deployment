bcf3403d483daaa902f1d89bf4f7b03ac4ee23b0
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-04-30 17:05:13 +0200

    Added env vars values for MixPanel and OneTrust (#474)

--- kubernetes/configs/prod/frontend.sh
+++ kubernetes/configs/prod/frontend.sh
@@ -3,7 +3,7 @@ SELFCARE_LOGIN_URL="https://selfcare.pagopa.it/auth/login"
 FRONTEND_SERVICE_PORT="80"
 PUBLIC_BUCKET_URL="https://interop-prod-public.s3.eu-central-1.amazonaws.com"
 MIXPANEL_PROJECT_ID="9a9131451cf2ea0a696854e2e57ebe3c"
-ONETRUST_DOMAIN_SCRIPT_ID="752f9d8a-9fb5-4194-a03d-cd403b5ef38d"
+ONETRUST_DOMAIN_SCRIPT_ID="5ab6d099-448b-4d79-985f-6c0f22382751"
 SELFCARE_BASE_URL="https://selfcare.pagopa.it"
 
 FRONTEND_RESOURCE_CPU="250m"
