1395e3c8b386375b319c02e8464612f30ec1155e
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-06-03 16:29:57 +0200

    dev-refactor: update purpose configs

--- kubernetes/configs/dev-refactor/purpose_process.sh
+++ kubernetes/configs/dev-refactor/purpose_process.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PURPOSE_PROCESS_SERVICE_NAME="interop-be-purpose-process-refactor"
 PURPOSE_PROCESS_STORAGE_TYPE="S3"
 PURPOSE_PROCESS_STORAGE_CONTAINER="interop-application-documents-refactor-dev"
 PURPOSE_PROCESS_STORAGE_ENDPOINT=""
