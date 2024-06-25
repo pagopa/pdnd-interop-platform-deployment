ffbdbb4aa934203c1b7222c24065fe4445104f6c
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-06-25 09:06:07 +0200

    Update kubernetes/configs/dev-refactor/commons.sh
    Co-authored-by: Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>

--- kubernetes/configs/dev-refactor/commons.sh
+++ kubernetes/configs/dev-refactor/commons.sh
@@ -57,7 +57,7 @@ PRIVACY_NOTICES_CONTAINER="interop-privacy-notices-content-dev"
 
 PAGOPA_TENANT_ID="69e2865e-65ab-4e48-a638-2037a9ee2ee7"
 
-REPORT_SENDER="noreply@reports.interop.pagopa.it"
+REPORT_SENDER="noreply@reports.refactor.dev.interop.pagopa.it"
 
 SMTP_ADDRESS="email-smtp.eu-central-1.amazonaws.com"
 SMTP_PORT=465
