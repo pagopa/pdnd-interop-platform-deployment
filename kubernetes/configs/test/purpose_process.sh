bb6c6176b1ceb629b7a9640e7521cb321e32670a
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-07-04 12:23:13 +0200

    Update kubernetes/configs/test/purpose_process.sh
    Co-authored-by: Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>

--- kubernetes/configs/test/purpose_process.sh
+++ kubernetes/configs/test/purpose_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PURPOSE_PROCESS_SERVICE_NAME="interop-be-purpose-process"
-PURPOSE_PROCESS_STORAGE_CONTAINER="interop-application-documents-refactor-test"
+PURPOSE_PROCESS_STORAGE_CONTAINER="interop-application-documents-test"
 
 PURPOSE_PROCESS_RESOURCE_CPU="500m"
 PURPOSE_PROCESS_RESOURCE_MEM="2Gi"
