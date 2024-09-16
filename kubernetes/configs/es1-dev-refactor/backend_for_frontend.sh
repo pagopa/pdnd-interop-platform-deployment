2573d8c738a91f4e69e0ea4975759c6de6a9bc36
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-09-16 16:59:10 +0200

    fix

--- kubernetes/configs/es1-dev-refactor/backend_for_frontend.sh
+++ kubernetes/configs/es1-dev-refactor/backend_for_frontend.sh
@@ -17,8 +17,8 @@ RISK_ANALYSIS_DOCS_CONTAINER="interop-application-documents-refactor-dev-es1"
 
 BFF_RATE_LIMITER_MAX_REQUESTS="9999999"
 BFF_RATE_LIMITER_BURST_PERCENTAGE="1.0"
-BFF_RATE_LIMITER_RATE_INTERVAL="1.second"
-BFF_RATE_LIMITER_TIMEOUT="300.milliseconds"
+BFF_RATE_LIMITER_RATE_INTERVAL=1000
+BFF_RATE_LIMITER_TIMEOUT=300
 
 SUPPORT_SAML_AUDIENCE="selfcare.refactor.dev.interop.pagopa.it"
 SUPPORT_SAML_CALLBACK_URL="https://selfcare.refactor.dev.interop.pagopa.it/ui/it/assistenza/scelta-ente"
