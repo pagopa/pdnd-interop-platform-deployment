8ba3ffc8fe3376a7881f6038ab8abe0a86cc7912
Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>
2024-09-16 15:48:18 +0200

    fix: frontend bucket URL (#594)

--- kubernetes/configs/prod/frontend.sh
+++ kubernetes/configs/prod/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SELFCARE_LOGIN_URL="https://selfcare.pagopa.it/auth/login"
 FRONTEND_SERVICE_PORT="80"
-PUBLIC_BUCKET_URL="https://interop-prod-public.s3.eu-central-1.amazonaws.com"
+PUBLIC_BUCKET_URL="https://interop-${ENVIRONMENT}-public${REGION_SUFFIX}.s3.${AWS_REGION}.amazonaws.com"
 MIXPANEL_PROJECT_ID="9a9131451cf2ea0a696854e2e57ebe3c"
 ONETRUST_DOMAIN_SCRIPT_ID="5ab6d099-448b-4d79-985f-6c0f22382751"
 SELFCARE_BASE_URL="https://selfcare.pagopa.it"
