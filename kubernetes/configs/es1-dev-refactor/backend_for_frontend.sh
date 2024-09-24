3ca14dc373478b37c5bc1cd75bd5f243ca807c2b
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-09-24 15:16:52 +0200

    Add new bff deployment (#598)

--- kubernetes/configs/es1-dev-refactor/backend_for_frontend.sh
+++ kubernetes/configs/es1-dev-refactor/backend_for_frontend.sh
@@ -11,13 +11,14 @@ BACKEND_FOR_FRONTEND_RESOURCE_MEM="2Gi"
 
 CONSUMER_DOCS_CONTAINER="interop-application-documents-refactor-dev-es1"
 ESERVICE_DOCS_CONTAINER="interop-application-documents-refactor-dev-es1"
+ESERVICE_DOCUMENTS_PATH="eservices/docs"
 ALLOW_LIST_CONTAINER="interop-allow-list-dev-es1"
 RISK_ANALYSIS_DOCS_CONTAINER="interop-application-documents-refactor-dev-es1"
 
 BFF_RATE_LIMITER_MAX_REQUESTS="9999999"
 BFF_RATE_LIMITER_BURST_PERCENTAGE="1.0"
-BFF_RATE_LIMITER_RATE_INTERVAL="1.second"
-BFF_RATE_LIMITER_TIMEOUT="300.milliseconds"
+BFF_RATE_LIMITER_RATE_INTERVAL=1000
+BFF_RATE_LIMITER_TIMEOUT=300
 
 SUPPORT_SAML_AUDIENCE="selfcare.refactor.dev.interop.pagopa.it"
 SUPPORT_SAML_CALLBACK_URL="https://selfcare.refactor.dev.interop.pagopa.it/ui/it/assistenza/scelta-ente"
@@ -27,3 +28,6 @@ IMPORT_ESERVICE_CONTAINER="interop-application-import-export-dev-es1"
 EXPORT_ESERVICE_CONTAINER="interop-application-import-export-dev-es1"
 PRESIGNED_URL_GET_DURATION_MINUTES=2
 PRESIGNED_URL_PUT_DURATION_MINUTES=2
+
+PRIVACY_NOTICES_PATH="consent"
+PRIVACY_NOTICES_FILE_NAME="pp.json"
