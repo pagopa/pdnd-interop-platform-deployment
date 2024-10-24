f5b0b255d6b8ba30ab89c4b8d05c78c8d5f76393
Michele De Simone <michele.desimone@pagopa.it>
2024-10-24 17:19:59 +0200

    chore: remove eu-central-1 region configs

--- kubernetes/configs/dev-refactor/agreement_process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AGREEMENT_PROCESS_SERVICE_NAME="interop-be-agreement-process-refactor"
-AGREEMENT_PROCESS_STORAGE_CONTAINER="interop-application-documents-refactor-dev"
-
-AGREEMENT_PROCESS_RESOURCE_CPU="2"
-AGREEMENT_PROCESS_RESOURCE_MEM="4Gi"
--- kubernetes/configs/test/purpose_process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PURPOSE_PROCESS_SERVICE_NAME="interop-be-purpose-process"
-PURPOSE_PROCESS_STORAGE_CONTAINER="interop-application-documents-test"
-
-PURPOSE_PROCESS_RESOURCE_CPU="500m"
-PURPOSE_PROCESS_RESOURCE_MEM="2Gi"
