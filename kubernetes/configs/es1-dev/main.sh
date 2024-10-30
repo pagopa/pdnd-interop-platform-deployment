3f1dedbd799cfb9f7263315c99c284b7a311e500
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-10-30 10:20:10 +0100

    update dev

--- kubernetes/configs/es1-dev/main.sh
+++ kubernetes/configs/es1-dev/main.sh
@@ -35,16 +35,16 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-dev/attributes_loader.sh
 . $(pwd)/kubernetes/configs/es1-dev/backend_for_frontend.sh
 . $(pwd)/kubernetes/configs/es1-dev/catalog_readmodel_writer.sh
+. $(pwd)/kubernetes/configs/es1-dev/client_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/catalog_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/compute_agreements_consumer.sh
-. $(pwd)/kubernetes/configs/es1-dev/client_readmodel_writer.sh
-. $(pwd)/kubernetes/configs/es1-dev/key_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/frontend.sh
+. $(pwd)/kubernetes/configs/es1-dev/key_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/party_registry_proxy.sh
-. $(pwd)/kubernetes/configs/es1-dev/purpose_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/purpose_readmodel_writer.sh
-. $(pwd)/kubernetes/configs/es1-dev/tenant_process.sh
+. $(pwd)/kubernetes/configs/es1-dev/purpose_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/tenant_readmodel_writer.sh
+. $(pwd)/kubernetes/configs/es1-dev/tenant_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/tenants-certified-attributes-updater.sh
 . $(pwd)/kubernetes/configs/es1-dev/notifier.sh
 . $(pwd)/kubernetes/configs/es1-dev/notifier_seeder.sh
@@ -60,3 +60,5 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-dev/selfcare_onboarding_consumer.sh
 . $(pwd)/kubernetes/configs/es1-dev/eservice_descriptors_archiver.sh
 . $(pwd)/kubernetes/configs/es1-dev/datalake_data_export.sh
+. $(pwd)/kubernetes/configs/es1-dev/producer-key-readmodel-writer.sh
+. $(pwd)/kubernetes/configs/es1-dev/producer-keychain-readmodel-writer.sh
