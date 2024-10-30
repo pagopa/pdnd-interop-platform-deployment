695ff91c87f333aa86c1fe4cf1df1e8e4a21a7b6
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-10-30 11:38:43 +0100

    Update main.sh

--- kubernetes/configs/es1-dev/main.sh
+++ kubernetes/configs/es1-dev/main.sh
@@ -23,7 +23,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-dev/ses_mock.sh
 
 . $(pwd)/kubernetes/configs/es1-dev/agreement_email_sender.sh
-. $(pwd)/kubernetes/configs/es1-dev/agreemen_outbound_writer.sh
+. $(pwd)/kubernetes/configs/es1-dev/agreement_outbound_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/agreement_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/agreement_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/api_gateway.sh
