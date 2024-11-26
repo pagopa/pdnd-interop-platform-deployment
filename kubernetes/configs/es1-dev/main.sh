d37e0114965b0da5239e53c6def60073d7240a1c
Michele De Simone <michele.desimone@pagopa.it>
2024-11-26 09:39:39 +0100

    fix config

--- kubernetes/configs/es1-dev/main.sh
+++ kubernetes/configs/es1-dev/main.sh
@@ -40,7 +40,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-dev/catalog_process.sh
 . $(pwd)/kubernetes/configs/es1-dev/compute_agreements_consumer.sh
 . $(pwd)/kubernetes/configs/es1-dev/client_readmodel_writer.sh
-. $(pwd)/kubernetes/configs/es1-dev/datalake_interface_export.sh
+. $(pwd)/kubernetes/configs/es1-dev/datalake_interface_exporter.sh
 . $(pwd)/kubernetes/configs/es1-dev/key_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-dev/frontend.sh
 . $(pwd)/kubernetes/configs/es1-dev/party_registry_proxy.sh
