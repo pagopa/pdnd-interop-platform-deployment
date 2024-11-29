560aa527e0a53214dbafa181985b8d81fc4df5eb
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-11-29 11:08:55 +0100

    add att config

--- kubernetes/configs/es1-att/main.sh
+++ kubernetes/configs/es1-att/main.sh
@@ -40,6 +40,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-att/catalog_process.sh
 . $(pwd)/kubernetes/configs/es1-att/compute_agreements_consumer.sh
 . $(pwd)/kubernetes/configs/es1-att/client_readmodel_writer.sh
+. $(pwd)/kubernetes/configs/es1-att/datalake_interface_exporter.sh
 . $(pwd)/kubernetes/configs/es1-att/key_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-att/frontend.sh
 . $(pwd)/kubernetes/configs/es1-att/party_registry_proxy.sh
