274d5882a6354526cc1068f4bb7d68a9d5de451b
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2024-11-29 13:02:24 +0100

    IMN-517-auth-server: Authorization server in node

--- kubernetes/configs/es1-dev/authorization_server.sh
+++ kubernetes/configs/es1-dev/authorization_server.sh
@@ -14,3 +14,10 @@ AUTH_SERVER_RATE_LIMITER_RATE_INTERVAL="1.second"
 AUTH_SERVER_RATE_LIMITER_TIMEOUT="300.milliseconds"
 
 JWT_FALLBACK_BUCKET="interop-generated-jwt-details-fallback-dev-es1"
+
+
+################################################
+# Configurations for Node version
+################################################
+AUTHORIZATION_SERVER_NODE_RESOURCE_CPU="500m"
+AUTHORIZATION_SERVER_NODE_RESOURCE_MEM="2Gi"
