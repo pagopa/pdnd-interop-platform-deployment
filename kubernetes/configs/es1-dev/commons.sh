851b9dda998b49c2b6fdc013797ec3005a6a9ffa
Michele De Simone <michele.desimone@pagopa.it>
2024-12-03 14:31:41 +0100

    fix: delegation topic value

--- kubernetes/configs/es1-dev/commons.sh
+++ kubernetes/configs/es1-dev/commons.sh
@@ -28,6 +28,7 @@ AUTHORIZATION_TOPIC="event-store.dev_authorization.events"
 CATALOG_TOPIC="event-store.dev_catalog.events"
 PURPOSE_TOPIC="event-store.dev_purpose.events"
 TENANT_TOPIC="event-store.dev_tenant.events"
+DELEGATION_TOPIC="event-store.dev_delegation.events"
 
 AGREEMENT_OUTBOUND_TOPIC="outbound.dev_agreement.events"
 CATALOG_OUTBOUND_TOPIC="outbound.dev_catalog.events"
