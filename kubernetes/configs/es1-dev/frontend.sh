58134b0750c859f4a44df706422a0339b83f4320
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-12-09 16:33:40 +0100

    Fix typo (missing R) and fill env vars value
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>

--- kubernetes/configs/es1-dev/frontend.sh
+++ kubernetes/configs/es1-dev/frontend.sh
@@ -6,8 +6,8 @@ PUBLIC_BUCKET_URL="https://interop-${ENVIRONMENT}-public${REGION_SUFFIX}.s3.${AW
 MIXPANEL_PROJECT_ID="b6c8c3c3ed0b32d66c61593bcb84e705"
 ONETRUST_DOMAIN_SCRIPT_ID="018ef6c1-31f6-70a6-bf72-d7d45c0ade7e"
 SELFCARE_BASE_URL="https://uat.selfcare.pagopa.it"
-API_SIGNAL_HUB_PUSH_INTEFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/push-signals_1.0.0.yaml"
-API_SIGNAL_HUB_PULL_INTEFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/pull-signals_1.0.0.yaml"
+API_SIGNAL_HUB_PUSH_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/develop/docs/openAPI/push-signals.yaml"
+API_SIGNAL_HUB_PULL_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/develop/docs/openAPI/pull-signals.yaml"
 
 FRONTEND_RESOURCE_CPU="250m"
 FRONTEND_RESOURCE_MEM="1Gi"
--- kubernetes/configs/es1-prod/frontend.sh
+++ kubernetes/configs/es1-prod/frontend.sh
@@ -5,8 +5,8 @@ PUBLIC_BUCKET_URL="https://interop-${ENVIRONMENT}-public${REGION_SUFFIX}.s3.${AW
 MIXPANEL_PROJECT_ID="9a9131451cf2ea0a696854e2e57ebe3c"
 ONETRUST_DOMAIN_SCRIPT_ID="5ab6d099-448b-4d79-985f-6c0f22382751"
 SELFCARE_BASE_URL="https://selfcare.pagopa.it"
-API_SIGNAL_HUB_PUSH_INTEFACE_URL="fill_me"
-API_SIGNAL_HUB_PULL_INTEFACE_URL="fill_me"
+API_SIGNAL_HUB_PUSH_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/push-signals.yaml"
+API_SIGNAL_HUB_PULL_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/pull-signals.yaml" 
 
 FRONTEND_RESOURCE_CPU="250m"
 FRONTEND_RESOURCE_MEM="1Gi"
