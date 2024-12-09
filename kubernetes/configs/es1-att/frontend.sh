c1c8b3edd405359e8c8920346078e66bf2dec08d
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-12-09 16:28:33 +0100

    Update kubernetes/configs/es1-att/frontend.sh
    Co-authored-by: Paolo Manca <154322757+paolomanca-pagopa@users.noreply.github.com>

--- kubernetes/configs/es1-att/frontend.sh
+++ kubernetes/configs/es1-att/frontend.sh
@@ -6,8 +6,8 @@ PUBLIC_BUCKET_URL="https://interop-${ENVIRONMENT}-public${REGION_SUFFIX}.s3.${AW
 MIXPANEL_PROJECT_ID="fill_me"
 ONETRUST_DOMAIN_SCRIPT_ID="fill_me"
 SELFCARE_BASE_URL="https://selfcare.pagopa.it"
-API_SIGNAL_HUB_PUSH_INTEFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/push-signals_1.0.0.yaml"
-API_SIGNAL_HUB_PULL_INTEFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/pull-signals_1.0.0.yaml"
+API_SIGNAL_HUB_PUSH_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/push-signals.yaml"
+API_SIGNAL_HUB_PULL_INTERFACE_URL="https://raw.githubusercontent.com/pagopa/interop-signalhub-core/refs/heads/main/docs/openAPI/pull-signals.yaml"
 
 FRONTEND_RESOURCE_CPU="250m"
 FRONTEND_RESOURCE_MEM="1Gi"
