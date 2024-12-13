f72aabe7627dfcc91783042e666424d5a9e26e7a
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-12-13 12:40:21 +0100

    add delegation in att

--- kubernetes/configs/es1-att/main.sh
+++ kubernetes/configs/es1-att/main.sh
@@ -42,6 +42,8 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-att/client_purpose_updater.sh
 . $(pwd)/kubernetes/configs/es1-att/client_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-att/datalake_interface_exporter.sh
+. $(pwd)/kubernetes/configs/es1-att/delegation_process.sh 
+. $(pwd)/kubernetes/configs/es1-att/delegation_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-att/key_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-att/frontend.sh
 . $(pwd)/kubernetes/configs/es1-att/party_registry_proxy.sh
