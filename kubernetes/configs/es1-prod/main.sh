9b859f8f49c269c401349ca037147f6e82c3b0ab
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2024-12-16 08:20:59 +0100

    Add: - update eservice attribute - new client_purpose_updater consumer - new agreement notifications

--- kubernetes/configs/es1-prod/main.sh
+++ kubernetes/configs/es1-prod/main.sh
@@ -35,6 +35,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-prod/catalog_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-prod/catalog_process.sh
 . $(pwd)/kubernetes/configs/es1-prod/compute_agreements_consumer.sh
+. $(pwd)/kubernetes/configs/es1-test/client_purpose_updater.sh
 . $(pwd)/kubernetes/configs/es1-prod/client_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-prod/datalake_interface_exporter.sh
 . $(pwd)/kubernetes/configs/es1-prod/key_readmodel_writer.sh
