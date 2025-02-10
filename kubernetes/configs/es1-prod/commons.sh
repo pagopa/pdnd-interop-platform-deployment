3b034c4b42381848326fd910224e514a28707d5e
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2025-02-10 11:59:34 +0100

    feat: add signalhub whitelist in prod

--- kubernetes/configs/es1-prod/commons.sh
+++ kubernetes/configs/es1-prod/commons.sh
@@ -88,3 +88,7 @@ SMTP_PORT=465
 
 AUTHORIZATION_MANAGEMENT_URL="http://interop-be-authorization-management.prod.svc.cluster.local:8088/authorization-management/1.0"
 CLIENT_ASSERTION_JWT_AUDIENCE="auth.interop.pagopa.it/client-assertion"
+
+FEATURE_FLAG_SIGNALHUB_WHITELIST=true
+SIGNALHUB_WHITELIST_PRODUCER=84871fd4-2fd7-46ab-9d22-f6b452f4b3c5 # PagoPA
+SIGNALHUB_WHITELIST_CONSUMER="84871fd4-2fd7-46ab-9d22-f6b452f4b3c5"  # PagoPA
