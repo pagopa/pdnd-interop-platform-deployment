ebe7eaa76e9d0e1a34783a967906414f427b4fa9
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2025-02-11 10:56:08 +0100

    PIN-5794: Use string in env var

--- kubernetes/configs/es1-dev/ipa_certified_attributes_importer.sh
+++ kubernetes/configs/es1-dev/ipa_certified_attributes_importer.sh
@@ -5,7 +5,7 @@ IPA_INSTITUTIONS_URL="https://indicepa.gov.it/ipa-dati/datastore/dump/d09adf99-d
 IPA_AOO_URL="https://indicepa.gov.it/ipa-dati/datastore/dump/cdaded04-f84e-4193-a720-47d6d5f422aa?format=json"
 IPA_UO_URL="https://indicepa.gov.it/ipa-dati/datastore/dump/b0aa1f6c-f135-4c8a-b416-396fed4e1a5d?format=json"
 
-IPA_CERTIFIED_ATTRIBUTES_IMPORTER_ATTRIBUTE_CREATION_WAIT_MS=5000
+IPA_CERTIFIED_ATTRIBUTES_IMPORTER_ATTRIBUTE_CREATION_WAIT_MS="5000"
 IPA_CERTIFIED_ATTRIBUTES_IMPORTER_JWT_DURATION_SECONDS="1800"
 
 JOB_IPA_CERTIFIED_ATTRIBUTES_IMPORTER_RESOURCE_CPU="500m"
