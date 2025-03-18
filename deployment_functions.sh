6337641460c405a055798ba6957d183c46c27a91
Michele De Simone <michele.desimone@pagopa.it>
2025-03-18 11:32:51 +0100

    feat: rollout graceful connection draining

--- deployment_functions.sh
+++ deployment_functions.sh
@@ -278,6 +278,7 @@ function createIngress() {
   --annotation="alb.ingress.kubernetes.io/scheme=internal"
   --annotation="alb.ingress.kubernetes.io/target-type=ip"
   --annotation="alb.ingress.kubernetes.io/group.name=interop-be"
+  --annotation="alb.ingress.kubernetes.io/target-group-attributes=deregistration_delay.timeout_seconds=31"
   --annotation="alb.ingress.kubernetes.io/load-balancer-attributes=routing.http.preserve_host_header.enabled=true"
 EOT
 )
