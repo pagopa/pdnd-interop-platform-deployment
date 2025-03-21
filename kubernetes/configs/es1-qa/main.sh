585bdf8eed28aca67a46efbac25335e1b44d06d1
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2025-03-21 10:05:17 +0100

    PIN-5794: ipa-certified-attributes-importer in qa (#735)

--- kubernetes/configs/es1-qa/main.sh
+++ kubernetes/configs/es1-qa/main.sh
@@ -33,7 +33,6 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-qa/authorization_updater.sh
 . $(pwd)/kubernetes/configs/es1-qa/attribute_registry_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-qa/attribute_registry_process.sh
-. $(pwd)/kubernetes/configs/es1-qa/attributes_loader.sh
 . $(pwd)/kubernetes/configs/es1-qa/backend_for_frontend.sh
 . $(pwd)/kubernetes/configs/es1-qa/catalog_readmodel_writer.sh
 . $(pwd)/kubernetes/configs/es1-qa/catalog_platformstate_writer.sh
@@ -52,7 +51,6 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-qa/purpose_process.sh
 . $(pwd)/kubernetes/configs/es1-qa/tenant_process.sh
 . $(pwd)/kubernetes/configs/es1-qa/tenant_readmodel_writer.sh
-. $(pwd)/kubernetes/configs/es1-qa/tenants-certified-attributes-updater.sh
 . $(pwd)/kubernetes/configs/es1-qa/notification_email_sender.sh
 . $(pwd)/kubernetes/configs/es1-qa/notifier.sh
 . $(pwd)/kubernetes/configs/es1-qa/notifier_seeder.sh
@@ -60,6 +58,7 @@ NOTIFIER_INTERFACE_VERSION=$(shortVersion $NOTIFIER_IMAGE_VERSION)
 . $(pwd)/kubernetes/configs/es1-qa/party_registry_proxy_refresher.sh
 . $(pwd)/kubernetes/configs/es1-qa/anac_certified_attributes_importer.sh
 . $(pwd)/kubernetes/configs/es1-qa/ivass_certified_attributes_importer.sh
+. $(pwd)/kubernetes/configs/es1-qa/ipa_certified_attributes_importer.sh
 . $(pwd)/kubernetes/configs/es1-qa/pn_consumers.sh
 . $(pwd)/kubernetes/configs/es1-qa/padigitale_report_generator.sh
 . $(pwd)/kubernetes/configs/es1-qa/dtd_catalog_exporter.sh
